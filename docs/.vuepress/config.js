7d03dae395420cdce31b4242c12122b25ec03110
KakiageSeiro <frederika07_bernkastel@yahoo.co.jp>
2019-10-01 01:18:55 +0900

    ボタンページを追加

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,15 @@
 module.exports = {
-  themeConfig: {
-    sidebar: [
-      '/',
-      '/buttons' //h1,h2が表示される項目名になります
-      //下記の記述だとh1として表示される項目名を指定できます
-      //['/buttons', 'ボタン']
-    ]
-  }
+    base: '/styleguide/',
+    head: [
+        ['link', { rel: 'stylesheet', href: '/styles/adjustment-layout.css' }],
+        ['link', { rel: 'stylesheet', href: '/styles/main.css' }]
+    ],
+    themeConfig: {
+        sidebar: [
+            '/',
+            '/buttons' // h1, h2が表示される項目名になります
+            // 下記の記述だと h1 として表示される項目名を指定できます
+            // ['/buttons', 'ボタン']
+        ]
+    }
 }
