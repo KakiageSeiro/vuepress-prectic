1d6b3f812359cc7a0839e9a54ee6d5e32772be10
KakiageSeiro <frederika07_bernkastel@yahoo.co.jp>
2019-10-01 00:34:09 +0900

    ボタンページとサイドバーを作成

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  themeConfig: {
+    sidebar: [
+      '/',
+      '/buttons' //h1,h2が表示される項目名になります
+      //下記の記述だとh1として表示される項目名を指定できます
+      //['/buttons', 'ボタン']
+    ]
+  }
+}
